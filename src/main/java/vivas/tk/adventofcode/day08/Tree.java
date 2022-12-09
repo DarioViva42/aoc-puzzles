fc53abdc2199a48194f6df50ca8c5155bd7e460f
Dario Viva <darioviva@gmail.com>
2022-12-09 02:32:31 +0100

    make comparable statement more readable

--- src/main/java/vivas/tk/adventofcode/day08/Tree.java
+++ src/main/java/vivas/tk/adventofcode/day08/Tree.java
@@ -3,6 +3,6 @@ package vivas.tk.adventofcode.day08;
 public record Tree(byte x, byte y, byte height) implements Comparable<Tree> {
     @Override
     public int compareTo(Tree other) {
-        return y - other.y != 0 ? y - other.y : x - other.x;
+        return y != other.y ? y - other.y : x - other.x;
     }
 }
