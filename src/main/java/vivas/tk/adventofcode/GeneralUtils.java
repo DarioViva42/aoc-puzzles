0ee597117387c38c61d03a8a2a6c464d46463552
Dario Viva <darioviva@gmail.com>
2022-12-09 21:18:39 +0100

    read puzzle input from server when file is not found locally

--- src/main/java/vivas/tk/adventofcode/GeneralUtils.java
+++ src/main/java/vivas/tk/adventofcode/GeneralUtils.java
@@ -6,30 +6,61 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.Optional;
 
 public class GeneralUtils {
 
+    static {
+        loadSecurityProperties();
+    }
+
     private static final StackWalker STACK_WALKER = StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE);
+    private static final String LOCAL_PATH = "/day%s";
+    private static final String SERVER_PATH = "https://adventofcode.com/%d/day/%d/%s";
 
     private GeneralUtils() {
     }
 
     public static String readPuzzleInput() {
+        AdventDate date = AdventDate.fromClass(STACK_WALKER.getCallerClass());
 
-        String[] tokens = STACK_WALKER.getCallerClass()
-                .getPackageName().split("\\.");
-        String day = tokens[tokens.length - 1];
-
-        URL resource = localResource(day);
-        try (InputStream inputStream = resource.openStream()) {
+        try (InputStream inputStream = getResource(date)) {
             return IOUtils.toString(inputStream, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    private static InputStream getResource(AdventDate date) {
+        return getResourceFromFile(date)
+                .or(() -> getResourceFromServer(date))
+                .orElseThrow();
+    }
+
+    private static Optional<InputStream> getResourceFromFile(AdventDate date) {
+        String path = LOCAL_PATH.formatted(StringUtils.leftPad(String.valueOf(date.day()), 2));
+        InputStream inputStream = GeneralUtils.class.getResourceAsStream(path);
+        return inputStream != null ? Optional.of(inputStream) : Optional.empty();
+    }
+
+    private static Optional<InputStream> getResourceFromServer(AdventDate date) {
+        try {
+            URL url = new URL(SERVER_PATH.formatted(date.year(), date.day(), "input"));
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestProperty("Cookie", "session=" + System.getProperty("aoc.session"));
+            return Optional.of(connection.getInputStream());
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return "";
+        return Optional.empty();
     }
 
-    private static URL localResource(String day) {
-        return GeneralUtils.class.getResource("/" + day);
+    private static void loadSecurityProperties() {
+        InputStream is = GeneralUtils.class.getResourceAsStream("/security.properties");
+        try {
+            System.getProperties().load(is);
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
     }
 }
