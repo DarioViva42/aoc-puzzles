f6d082f3f03a39f40d31d4ddd4bff1b8793e0830
Dario Viva <darioviva@gmail.com>
2023-12-09 16:25:40 +0100

    NetworkDocumentTest: fix import for assertThat

--- src/test/java/tk/vivas/adventofcode/year2023/day08/NetworkDocumentTest.java
+++ src/test/java/tk/vivas/adventofcode/year2023/day08/NetworkDocumentTest.java
@@ -3,7 +3,7 @@ package tk.vivas.adventofcode.year2023.day08;
 import org.junit.jupiter.api.Test;
 import tk.vivas.adventofcode.AocUtils;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class NetworkDocumentTest {
 
