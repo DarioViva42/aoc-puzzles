eec2f06ebf7bdfb60843b1e60258f62281adf0b1
Dario Viva <darioviva@gmail.com>
2023-12-09 15:39:56 +0100

    add MathUtils for lcm (least common multiple) why on earth is this not implemented in java.lang.math?

---
+++ src/main/java/tk/vivas/MathUtils.java
@@ -0,0 +1,34 @@
+package tk.vivas;
+
+public class MathUtils {
+
+    private MathUtils() {
+    }
+
+    public static long lcm(long... numbers) {
+        if (numbers.length == 0) {
+            return 1;
+        }
+        if (numbers.length == 1) {
+            return numbers[0];
+        }
+        long lcm = 1;
+        for (long number : numbers) {
+            lcm = lcm(lcm, number);
+        }
+        return lcm;
+    }
+
+    public static long lcm(long a, long b) {
+        if (a == 0 || b == 0) {
+            return 0;
+        }
+        long min = Math.min(a, b);
+        long max = Math.max(a, b);
+        long lcm = max;
+        while (lcm % min != 0L) {
+            lcm += max;
+        }
+        return lcm;
+    }
+}
