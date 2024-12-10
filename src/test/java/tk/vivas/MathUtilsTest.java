e32857a3d5e0aa7492264ad8d6daebf533ea258e
viva2 <dario.viva@coop.ch>
2024-12-10 14:10:47 +0100

    MathUtils: add test for concatenate method

--- src/test/java/tk/vivas/MathUtilsTest.java
+++ src/test/java/tk/vivas/MathUtilsTest.java
@@ -1,6 +1,8 @@
 package tk.vivas;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -18,5 +20,17 @@ class MathUtilsTest {
         long lcm = MathUtils.lcm(12, 18);
 
         assertThat(lcm).isEqualTo(36);
+    }
+
+    @ParameterizedTest(name = "{0} || {1} == {2}")
+    @CsvSource(value = {
+            "4,2,42",
+            "10,45,1045",
+            "627,91,62791",
+            "51,203,51203"
+    })
+    void concatenate(int a, int b, int expected) {
+        assertThat(MathUtils.concatenate(a, b))
+                .isEqualTo(expected);
     }
 }
