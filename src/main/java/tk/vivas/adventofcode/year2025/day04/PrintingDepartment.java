30f1322d7cac98b1b53af433b0d7218d00ba11da
DarioViva42 <darioviva@gmail.com>
2025-12-04 22:29:07 +0100

    2025 day 04: find out how many rolls of paper can be removed

--- src/main/java/tk/vivas/adventofcode/year2025/day04/PrintingDepartment.java
+++ src/main/java/tk/vivas/adventofcode/year2025/day04/PrintingDepartment.java
@@ -13,33 +13,52 @@ class PrintingDepartment {
     private final int sizeX;
     private final int sizeY;
 
-    PrintingDepartment(String input) {
-        map = input.lines().map(line -> line.chars()
-                        .mapToObj(c -> Tile.of((char) c))
-                        .toList())
-                .toList();
+    private PrintingDepartment(List<List<Tile>> map) {
+        this.map = map;
 
         sizeX = map.getFirst().size();
         sizeY = map.size();
     }
 
+    PrintingDepartment(String input) {
+        this(parse(input));
+    }
+
+    private static List<List<Tile>> parse(String input) {
+        return input.lines().map(line -> line.chars()
+                        .mapToObj(c -> Tile.of((char) c))
+                        .toList())
+                .toList();
+    }
+
     long accessibleRollsOfPaper() {
-        return IntStream.range(0, sizeY)
-                .mapToObj(y -> IntStream.range(0, sizeX)
-                        .mapToObj(x -> new Position(x, y)))
-                .flatMap(Function.identity())
+        return positions()
                 .filter(this::isRollOfPaper)
                 .filter(this::isAccessible)
                 .count();
+    }
+
+    long removableRollsOfPaper() {
+        long initialAmount = countPapers();
+
+        PrintingDepartment currentState = this;
+        while (true) {
+            PrintingDepartment nextState = currentState.nextState();
+
+            if (nextState.equals(currentState)) {
+                break;
+            }
+
+            currentState = nextState;
+        }
+        return initialAmount - currentState.countPapers();
     }
 
     private boolean isRollOfPaper(Position position) {
         return Tile.ROLL_OF_PAPER.equals(map.get(position.y()).get(position.x()));
     }
 
-    private boolean isAccessible(Position position) {
-        int x = position.x();
-        int y = position.y();
+    private boolean isAccessible(int x, int y) {
         return Stream.of(
                         tile(x - 1, y - 1), tile(x, y - 1), tile(x + 1, y - 1),
                         tile(x - 1, y),        /* center paper */ tile(x + 1, y),
@@ -49,10 +68,61 @@ class PrintingDepartment {
                 .count() < 4;
     }
 
+    private boolean isAccessible(Position position) {
+        return isAccessible(position.x(), position.y());
+    }
+
+    private PrintingDepartment nextState() {
+        List<List<Tile>> nextMap = IntStream.range(0, sizeY)
+                .mapToObj(y -> IntStream.range(0, sizeX)
+                        .mapToObj(x -> nextState(x, y))
+                        .toList())
+                .toList();
+        return new PrintingDepartment(nextMap);
+    }
+
+    private Tile nextState(int x, int y) {
+        Tile tile = tile(x, y);
+        if (tile == Tile.EMPTY) {
+            return Tile.EMPTY;
+        }
+        return isAccessible(x, y) ? Tile.EMPTY : Tile.ROLL_OF_PAPER;
+    }
+
+    private long countPapers() {
+        return positions()
+                .map(this::tile)
+                .filter(Tile.ROLL_OF_PAPER::equals)
+                .count();
+    }
+
     private Tile tile(int x, int y) {
         if (x >= sizeX || y >= sizeY || x < 0 || y < 0) {
             return Tile.EMPTY;
         }
         return map.get(y).get(x);
+    }
+
+    private Tile tile(Position position) {
+        return tile(position.x(), position.y());
+    }
+
+    private Stream<Position> positions() {
+        return IntStream.range(0, sizeY)
+                .mapToObj(y -> IntStream.range(0, sizeX)
+                        .mapToObj(x -> new Position(x, y)))
+                .flatMap(Function.identity());
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (!(o instanceof PrintingDepartment that)) return false;
+
+        return map.equals(that.map);
+    }
+
+    @Override
+    public int hashCode() {
+        return map.hashCode();
     }
 }
--- src/test/java/tk/vivas/adventofcode/year2025/day04/PrintingDepartmentTest.java
+++ src/test/java/tk/vivas/adventofcode/year2025/day04/PrintingDepartmentTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import tk.vivas.adventofcode.AocUtils;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class PrintingDepartmentTest {
 
@@ -15,5 +14,14 @@ class PrintingDepartmentTest {
         PrintingDepartment department = new PrintingDepartment(input);
 
         assertThat(department.accessibleRollsOfPaper()).isEqualTo(13);
+    }
+
+    @Test
+    void removableRollsOfPaper() {
+        String input = AocUtils.readPuzzleInput();
+
+        PrintingDepartment department = new PrintingDepartment(input);
+
+        assertThat(department.removableRollsOfPaper()).isEqualTo(43);
     }
 }
