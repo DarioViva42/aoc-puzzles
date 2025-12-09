9625735b2e274e0bfe11c10cac0c92dcfeac3b62
DarioViva42 <darioviva@gmail.com>
2025-12-09 20:54:39 +0100

    2025 day 08: calculate distance only once per edge

--- src/main/java/tk/vivas/adventofcode/year2025/day08/JunctionEdge.java
+++ src/main/java/tk/vivas/adventofcode/year2025/day08/JunctionEdge.java
@@ -1,8 +1,9 @@
 package tk.vivas.adventofcode.year2025.day08;
 
-record JunctionEdge(JunctionBox a, JunctionBox b) {
-    long squaredDistance() {
-        return a.squaredDistance(b);
+record JunctionEdge(JunctionBox a, JunctionBox b, long squaredDistance) {
+
+    JunctionEdge(JunctionBox a, JunctionBox b) {
+        this(a, b, a.squaredDistance(b));
     }
 
     int connect() {
